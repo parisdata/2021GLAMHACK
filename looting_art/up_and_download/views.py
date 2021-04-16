31d68a14a6f6956bffb0e535696121dd20144be3
Rae Knowler <daisywheel22@gmail.com>
2021-04-16 21:06:52 +0200

    Standardise file paths and tidy up a bit

--- looting_art/up_and_download/views.py
+++ looting_art/up_and_download/views.py
@@ -1,30 +1,41 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseServerError, HttpResponseNotAllowed
 from django.shortcuts import render
 import os
 from django.conf import settings
-import csv
 
 from .forms import UploadFileForm
 
 import subprocess
 
+COUNTING_SCRIPT = os.path.join(settings.BASE_DIR, 'up_and_download/scripts/counting.py')
+INPUT = os.path.join(settings.BASE_DIR, 'up_and_download/data/input.csv')
+OUTPUT = os.path.join(settings.BASE_DIR, 'up_and_download/data/output_flagged.csv')
+RF_PHRASES = os.path.join(settings.BASE_DIR, 'up_and_download/data/red-flags-phrases-full.csv')
+RF_NAMES = os.path.join(settings.BASE_DIR, 'up_and_download/data/red-flags-names-full.csv')
+
 column = ''
 
+
 def flag(file, col):
-    cmd = 'python3 ./up_and_download/scripts/counting.py ' + file +\
-          ' looting_art_prototype/output_flagged.csv --col ' + col
+    cmd = 'python3 {script} {input} {output} --col {col}' \
+          ' --rfphrase {rfphrase} --rfname {rfname}'.format(
+        script=COUNTING_SCRIPT, input=file, output=OUTPUT, col=col,
+        rfphrase=RF_PHRASES, rfname=RF_NAMES
+    )
     subprocess.call(cmd, shell=True)
 
+
 def returnDownload(file, col):
-    with open('output_test.csv', 'wb+') as destination:
+    with open(INPUT, 'wb+') as destination:
         for chunk in file.chunks():
             destination.write(chunk)
-    flag('output_test.csv', col)
+    flag(INPUT, col)
 
 
 # Index.html
 def index(request):
     return render(request, 'index.html')
+
 
 # Upload file and give column to check/flag
 def upload_file(request):
@@ -34,14 +45,12 @@ def upload_file(request):
             column = request.POST.get('column')
             returnDownload(request.FILES['testCSV'], column)
 
-            path = 'looting_art_prototype/output_flagged.csv'
-            file_path = os.path.join(settings.BASE_DIR, path)
-            if os.path.exists(file_path):
-                with open(file_path, 'r') as fh:
-                    response = HttpResponse(fh.read(), content_type="text/csv")
-                    response['Content-Disposition'] = 'attachment; filename=test.csv'
-                    return response
-            raise Http404
-    else:
-        return HttpResponse('ERROR: somethong went wrong...')
+            if not os.path.exists(OUTPUT):
+                raise HttpResponseServerError
 
+            with open(OUTPUT, 'r') as fh:
+                response = HttpResponse(fh.read(), content_type="text/csv")
+                response['Content-Disposition'] = 'attachment; filename=test.csv'
+                return response
+    else:
+        return HttpResponseNotAllowed(['POST'])
