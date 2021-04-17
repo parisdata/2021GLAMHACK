c94cbc2b5c05fc343f8f27245d79f8516ab84fb6
Rae Knowler <daisywheel22@gmail.com>
2021-04-17 10:13:04 +0200

    Move styles into css and serve static files

--- looting_art/looting_art/settings.py
+++ looting_art/looting_art/settings.py
@@ -117,3 +117,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'up_and_download/static/up_and_download')
+]
